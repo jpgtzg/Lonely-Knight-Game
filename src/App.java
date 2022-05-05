14faf11c93cdc55f4390ae2468a050eba20ce2b0
Dread5 <58833781+AndromedaHelix@users.noreply.github.com>
2022-05-05 10:36:21 -0500

    Revert "Added a quest System and Traebourne town"
    This reverts commit 4ee9ffe05a5be2a219635727333180d73344be77.

--- src/App.java
+++ src/App.java
@@ -7,7 +7,6 @@ public class App {
 
     //Game Variables)
     static Player player;
-    public static QuestManager questManager = new QuestManager();
 
     public static void main(String[] args) throws Exception {
         intro();
