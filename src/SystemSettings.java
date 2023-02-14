d16af8ca5ad1252025abda1b2633c88c4bfff4d8
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-02-14 11:18:58 -0600

    Removed Exception

--- src/SystemSettings.java
+++ src/SystemSettings.java
@@ -5,18 +5,22 @@
 
 public class SystemSettings {
 
-    public static void clear()
-    {
+    public static void clear() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
-    public static void print(String text) throws InterruptedException
-    {
+
+    public static void print(String text) {
         char chars[] = text.toCharArray();
         for (char letter : chars) {
             System.out.print(letter);
-            Thread.sleep(50);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         }
-        System.out.println("\n");  
+        System.out.println("\n");
     }
 }
