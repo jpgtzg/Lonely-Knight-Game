8298a14d4c4110ee42c1541b2bd050a4edfb9ca8
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-02-14 11:19:41 -0600

    Implemented ChoiceManager

--- src/App.java
+++ src/App.java
@@ -1,48 +1,35 @@
 import java.util.Scanner;
 
-public class App extends SystemSettings{
-    //Dialogue variables
+public class App extends SystemSettings {
+    // Dialogue variables
 
     static Scanner in = new Scanner(System.in);
 
-    //Game Variables)
+    // Game Variables)
     static Player player;
 
-    public static void main(String[] args) throws Exception 
-    {
+    public static void main(String[] args) {
         clear();
         print("Welcome, player. What's your name?");
-        player = new Player(in.nextLine(), 100, 100 );
+        player = new Player(in.nextLine(), 100, 100);
         clear();
         player.getName();
 
-        //Decision loop
-        boolean decision = true;
-        
-        //
-
         print("Excellent");
-        //Decision loop
-        decision = true;
-        while(decision){
-            print("Ready to play?(y/n)");
-            String choice = in.nextLine();
-            if(choice.equals("y"))
-            {
-                decision = false;
-                Traebourne.main(null);
-            }
-            else if(choice.equals("n"))
-            {
-                decision = false;
-                print("Goodbye.");
-                Thread.sleep(1000);
-                System.exit(0);
-            }
-            else
-            {
-                print("Please enter a valid choice.");
-            }
-        }
+        // Decision loop
+        ChoiceManager.makeChoiceYN("Ready to play?(y/n)",
+                () -> {
+                    Traebourne.main(null);
+                }, () -> {
+                    print("Goodbye.");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.exit(0);
+                });
+        
     }
 }
