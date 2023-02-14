f7947326d1a482916ba04c680eaeec3a101d68b3
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-02-14 11:19:04 -0600

    Created ChoiceManager

---
+++ src/ChoiceManager.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class ChoiceManager extends SystemSettings {
+
+    private static Scanner in = new Scanner(System.in);
+
+    public static Runnable makeChoiceInt(String choiceText, int numChoices, Runnable... method1Runnables) {
+
+        boolean decision = true;
+        while (decision) {
+            print(choiceText);
+            int choice = in.nextInt();
+
+            switch (choice) {
+                case 1:
+                    decision = false;
+                    method1Runnables[0].run();
+                    break;
+                case 2:
+                    decision = false;
+                    method1Runnables[1].run();
+                    break;
+                case 3:
+                    decision = false;
+                    method1Runnables[3].run();
+                    break;
+
+                default:
+                    print("Please enter a valid choice");
+                    break;
+            }
+        }
+
+        return null;
+    }
+
+    public static Runnable makeChoiceYN(String choiceText, Runnable... method1Runnables) {
+
+        boolean decision = true;
+        while (decision) {
+            print(choiceText);
+            String choice = in.nextLine();
+
+            switch (choice) {
+                case "y":
+                    decision = false;
+                    method1Runnables[0].run();
+                    break;
+                case "n":
+                    decision = false;
+                    method1Runnables[1].run();
+                    break;
+                default:
+                    print("Please enter a valid choice");
+                    break;
+            }
+        }
+
+        return null;
+    }
+
+}
