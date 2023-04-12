34d47410e99de2ba3c7bd426fbaea39d94bd7fd7
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-04-12 14:43:11 -0600

    Improved decision

--- src/game/Traebourne.java
+++ src/game/Traebourne.java
@@ -1,8 +1,16 @@
+/**
+ * Written by Juan Pablo Gutiérrez
+ */
+
 package game;
+
 import java.util.Scanner;
 
+import game.indoor.Shop;
+import game.indoor.Shop.Personalities;
 import gamemanager.App;
 import gamemanager.Player;
+import system.ChoiceManager;
 import system.SystemSettings;
 
 public class Traebourne extends SystemSettings {
@@ -13,6 +21,8 @@ public class Traebourne extends SystemSettings {
     // Game variables
     private static Player player = App.player;
 
+    private static Shop tShop = new Shop(player, Personalities.OLD);
+
     public static void main(String[] args) {
         clear();
         print("You are in Traebourne Town."
@@ -20,45 +30,29 @@ public class Traebourne extends SystemSettings {
                 + "\nAt your left there's a sign that reads 'Traebourne Local Shop'."
                 + "\nAt your right there's a sign that reads 'Traebourne Armory'.");
 
-        // Decision loop
-        boolean decision = true;
-        while (decision) {
-            print("Where would you like to go? "
-                    + "\n 1. Inn"
-                    + "\n 2. Shop"
-                    + "\n 3. Armory");
-
-            int choice = in.nextInt();
-            switch (choice) {
-                case 1:
-                    decision = false;
+        ChoiceManager.makeChoiceInt("Where would you like to go? "
+                + "\n 1. Inn"
+                + "\n 2. Shop"
+                + "\n 3. Armory", () -> {
                     inn();
-                    break;
-                case 2:
-                    decision = false;
+                }, () -> {
                     shop();
-                    break;
-                case 3:
-                    decision = false;
+                }, () -> {
                     armory();
-                    break;
-                default:
-                    print("Please enter a valid choice.");
-                    break;
-            }
-        }
-
+                });
     }
 
     private static void inn() {
-        print("You enter the Inn. The Innkeeper greets you.\nThe Inn is built out of wood and stone.");
+        print("You enter the Inn. The Innkeeper greets you.\nThe Inn is built out of wood and stone.\nThere are not many people inside. A small fireplace is at your left, with 3 people talking around it");
     }
 
     private static void shop() {
+        tShop.run();
         // TODO add shop
     }
 
     private static void armory() {
+        System.out.println("Hello");
         // TODO add armory
     }
 }
