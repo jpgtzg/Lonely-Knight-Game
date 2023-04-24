213a8104829e8a27c87c4e368b0649b612f35cd6
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-04-24 14:01:36 -0600

    Created constants

---
+++ src/utils/Constants.java
@@ -0,0 +1,25 @@
+/**
+ * Written by Juan Pablo Gutiérrez
+ */
+
+package utils;
+
+/**
+ * Manages constants values and data
+ */
+public final class Constants {
+    public static class ShopConstants {
+        public static final String[] names = { "Sword of Destiny", "The Wand of Power", "Legendary Bow", "Magic Tome",
+                "Dragon's Scale Armor",
+                "Elven Cloak", "Ancient Staff", "Enchanted Shield", "Crystal Orb", "Mystic Robe" };
+        public static final String[] descriptions = { "A powerful weapon forged by the gods themselves",
+                "Unleash your magic with this wand of immense power", "The perfect bow for any skilled archer",
+                "A book of ancient spells and knowledge", "Armor made from the scales of a dragon",
+                "A cloak woven by elven weavers from the finest materials", "A staff imbued with ancient magic",
+                "A shield that can deflect any attack", "A crystal orb that reveals hidden secrets",
+                "A robe infused with mystical energies" };
+        public static final double[] prices = { 29.99, 49.99, 99.99, 149.99, 199.99, 249.99, 299.99, 349.99, 399.99,
+                449.99 };
+
+    }
+}
